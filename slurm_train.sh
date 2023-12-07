3def1562cd69bbfb8af8ee82a89aaced1a7e3101
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-12-07 22:08:00 +0100

    prevent memory exhaustion

--- slurm_train.sh
+++ slurm_train.sh
@@ -12,10 +12,10 @@
 # Load necessary modules
 conda activate neural-ddp
 
-export OMP_NUM_THREADS=40
+export OMP_NUM_THREADS=24
 
 # Run the script with torchrun
 srun -ul --gpus-per-task=1 python train_model.py \
-    --dataset "cosmo" --val_interval 20 --epochs 60 --n_workers 20 \
+    --dataset "cosmo" --val_interval 20 --epochs 60 --n_workers 12 \
     --batch_size 1 --model "graph_lam"
     # --load saved_models/graph_lam-4x64-11_15_22_38_47/last.ckpt --resume_run '3gio4mcv'
