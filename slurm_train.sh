150e0ba42cb2e043174386fe9258332ffde828de
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-12-09 18:37:06 +0100

    switch to Hi_Lam to reduce artifacts around nodes

--- slurm_train.sh
+++ slurm_train.sh
@@ -17,5 +17,5 @@ export OMP_NUM_THREADS=24
 # Run the script with torchrun
 srun -ul --gpus-per-task=1 python train_model.py \
     --dataset "cosmo" --val_interval 20 --epochs 40 --n_workers 12 \
-    --batch_size 1 --model "graph_lam"
+    --batch_size 1 --model "hi_lam" --graph "hierarchical"
     # --load saved_models/graph_lam-4x64-11_15_22_38_47/last.ckpt --resume_run '3gio4mcv'
