9ba76e01daa7f9f5e0113cb0c694ed8550a46f6b
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-12-26 06:56:01 +0100

    slurm improved for vertical levels

--- slurm_train_graphlam.sh
+++ slurm_train_graphlam.sh
@@ -9,12 +9,12 @@
 #SBATCH --error=lightning_logs/neurwp_err.log
 #SBATCH --mem=490G
 
-export PREPROCESS=false
+export PREPROCESS=true
 
 # Load necessary modules
 conda activate neural-ddp
 
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=12
 
 if [ "$PREPROCESS" = true ]; then
     srun -ul -N1 -n1 python create_static_features.py --boundaries 60
@@ -26,5 +26,5 @@ fi
 
 # Run the script with torchrun
 srun -ul --gpus-per-task=1 python train_model.py \
-    --dataset "cosmo" --val_interval 20 --epochs 40 --n_workers 4 --batch_size 12
+    --dataset "cosmo" --val_interval 20 --epochs 40 --n_workers 8 --batch_size 12
     # --load saved_models/graph_lam-4x64-11_15_22_38_47/last.ckpt --resume_run '3gio4mcv'
