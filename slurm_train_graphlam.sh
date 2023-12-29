835edb71bf99ef538d7aa1e75d89b913b59d6947
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-12-29 11:48:46 +0100

    prevent core dump, optimize slurm

--- slurm_train_graphlam.sh
+++ slurm_train_graphlam.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=4
 #SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=4
-#SBATCH --partition=normal
+#SBATCH --partition=a100-80gb
 #SBATCH --account=s83
 #SBATCH --output=lightning_logs/neurwp_out.log
 #SBATCH --error=lightning_logs/neurwp_err.log
@@ -15,18 +15,18 @@ export PREPROCESS=false
 conda activate neural-ddp
 
 export OMP_NUM_THREADS=16
+ulimit -c 0
 
 if [ "$PREPROCESS" = true ]; then
     srun -ul -N1 -n1 python create_static_features.py --boundaries 60
     srun -ul -N1 -n1 python create_mesh.py --dataset "cosmo"
     srun -ul -N1 -n1 python create_grid_features.py --dataset "cosmo"
     # This takes multiple hours!
-    srun -ul -N1 -n1 python create_parameter_weights.py --dataset "cosmo" --batch_size 12 --n_workers 8 --step_length 1
+    srun -ul -N1 -n1 python create_parameter_weights.py --dataset "cosmo" --batch_size 32 --n_workers 8 --step_length 1
 fi
 
 # Run the script with torchrun
 srun -ul --gpus-per-task=1 python train_model.py \
-    --dataset "cosmo" --val_interval 20 --epochs 40 --n_workers 8 --batch_size 8 
-    # --load wandb/run-20231226_083638-4f5sanqa/files/latest-v1.ckpt \
-    # --resume_opt_sched 1
-    # --resume_run '3gio4mcv'
+    --dataset "cosmo" --val_interval 20 --epochs 40 --n_workers 8 --batch_size 12 \
+    --load wandb/run-20231229_092005-tyx3mnnu/files/latest-v1.ckpt --resume_opt_sched 1 \
+    --resume_run 'tyx3mnnu'
