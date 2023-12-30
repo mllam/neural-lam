a914813d0933b7930ea6536282be6b2c61737573
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-12-30 07:32:05 +0100

    preventing core dumps in slurm

--- slurm_train.sh
+++ slurm_train.sh
@@ -8,14 +8,12 @@
 #SBATCH --output=lightning_logs/neurwp_out.log
 #SBATCH --error=lightning_logs/neurwp_err.log
 #SBATCH --mem=490G
-#SBATCH --export=ALL,SLURM_CORE_SPEC=0
+#SBATCH --core-spec=0
 
 export PREPROCESS=false
 
 # Load necessary modules
 conda activate neural-ddp
-
-export OMP_NUM_THREADS=12
 
 if [ "$PREPROCESS" = true ]; then
     srun -ul -N1 -n1 python create_static_features.py --boundaries 60
@@ -27,6 +25,6 @@ fi
 
 # Run the script with torchrun
 srun -ul --gpus-per-task=1 python train_model.py \
-    --dataset "cosmo" --val_interval 20 --epochs 40 --n_workers 12 --batch_size 12 \
-    --load wandb/run-20231229_192556-ax4mb1mq/files/latest-v1.ckpt --resume_opt_sched 1 \
+    --dataset "cosmo" --val_interval 20 --epochs 40 --n_workers 2 --batch_size 12 \
+    --load wandb/run-20231229_220825-ax4mb1mq/files/latest.ckpt --resume_opt_sched 1 \
     --resume_run 'ax4mb1mq'
