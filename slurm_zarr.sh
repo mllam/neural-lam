ce6d33279b4626ce06036e90ea3a31af131cf6f6
Simon Adamov <simon.adamov@meteoswiss.ch>
2024-03-23 06:12:20 +0100

    One zarr to rule them all

---
+++ slurm_zarr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --job-name=NLZarr
+#SBATCH --nodes=1
+#SBATCH --partition=postproc
+#SBATCH --account=s83
+#SBATCH --output=lightning_logs/zarr_out.log
+#SBATCH --error=lightning_logs/zarr_err.log
+#SBATCH --mem=410G
+#SBATCH --time=5-00:00:00
+#SBATCH --no-requeue
+
+# Load necessary modules
+conda activate neural-lam
+ulimit -c 0
+export OMP_NUM_THREADS=16
+
+# Run the script with torchrun
+srun -ul python create_zarr.py
