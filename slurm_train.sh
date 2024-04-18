096f484885fc5bae21353483702a0cc0ee76a617
Simon Adamov <simon.adamov@mailbox.org>
2024-04-18 16:51:19 +0200

    flexible sbatch for Tsa

--- slurm_train.sh
+++ slurm_train.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -l
 #SBATCH --job-name=NeurWP
 #SBATCH --account=s83
-#SBATCH --partition=normal
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --mem=400G
+#SBATCH --ntasks-per-node=8
+#SBATCH --partition=normal
+#SBATCH --mem=300G
 #SBATCH --no-requeue
 #SBATCH --output=lightning_logs/neurwp_out.log
 #SBATCH --error=lightning_logs/neurwp_err.log
 
-export PREPROCESS=false
+export PREPROCESS=true
 export NORMALIZE=false
+export DATASET="meps"
 
 # Load necessary modules
 conda activate neural-lam
@@ -19,13 +20,13 @@ if [ "$PREPROCESS" = true ]; then
     echo "Create static features"
     srun -ul -N1 -n1 python create_static_features.py --boundaries 60 --dataset "cosmo"
     echo "Creating mesh"
-    srun -ul -N1 -n1 python create_mesh.py --dataset "cosmo" --plot 1
+    srun -ul -N1 -n1 python create_mesh.py --dataset $DATASET --plot 1
     echo "Creating grid features"
-    srun -ul -N1 -n1 python create_grid_features.py --dataset "cosmo"
+    srun -ul -N1 -n1 python create_grid_features.py --dataset $DATASET
     if [ "$NORMALIZE" = true ]; then
         # This takes multiple hours!
         echo "Creating normalization weights"
-        srun -ul -N1 -n1 python create_parameter_weights.py --dataset "cosmo" --batch_size 32 --n_workers 8 --step_length 1
+        srun -ul -N1 -n1 python create_parameter_weights.py --dataset $DATASET --batch_size 32 --n_workers 8 --step_length 1
     fi
 fi
 
@@ -33,5 +34,5 @@ ulimit -c 0
 export OMP_NUM_THREADS=16
 
 # Run the script with torchrun
-srun -ul python train_model.py --dataset "cosmo" --val_interval 5 \
+srun -ul python train_model.py --dataset $DATASET --val_interval 5 \
     --epochs 10 --n_workers 6 --batch_size 8 --subset_ds 1
