472981b4164a02b3438d35cb0117e9f28e2247f9
Capucine Lechartre <capucine.lechartre@meteoswiss.ch>
2024-04-26 11:52:52 +0200

    working plot for temperature

--- grib_modifyer.py
+++ grib_modifyer.py
@@ -1,117 +1,64 @@
-"""Modifying a GRIB from Python."""
-
 import earthkit.data
+import numpy as np
+import pygrib
 from matplotlib import pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cf
-import numpy as np
-import pygrib
 from neural_lam import constants
-from neural_lam.rotate_grid import unrot_lat, unrot_lon, unrotate_latlon
 
+def plot_data(grb, title, ax, projection, vmin, vmax, color_map='plasma', num_contours=100):
+    """Plot the data using Cartopy with specified projection on given axis, using shared color scale."""
+    lats, lons = grb.latlons()
+    data = grb.values
 
-# def plot_data(grb, title, save_path):
-#     """Plot the data using Cartopy and save it to a file."""
-#     lats, lons = grb.latlons()
-#     data = grb.values
+    ax.add_feature(cf.BORDERS, linestyle='-', edgecolor='black')
+    ax.add_feature(cf.COASTLINE, linestyle='-', edgecolor='black')
+    ax.set_title(title)
 
-#     # Setting up the map projection and the contour plot
-#     fig, ax = plt.subplots(figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree()})
-#     ax.coastlines()
-#     contour = ax.contourf(lons, lats, data, levels=np.linspace(data.min(), data.max(), 100), cmap='viridis')
-#     ax.set_title(title)
-#     plt.colorbar(contour, ax=ax, orientation='vertical')
+    contour = ax.contourf(lons, lats, data, transform=projection(), levels=np.linspace(vmin, vmax, num_contours), cmap=color_map)
+    return contour
 
-#     # Save the plot to a file
-#     plt.savefig(save_path, bbox_inches='tight')
-#     plt.close(fig)
-
+def create_modified_grib(original_data, cut_values, modified_file_name):
+    # Save the overwritten data
+    md = original_data.metadata()
+    data_new = earthkit.data.FieldList.from_array(cut_values, md)
+    data_new.save(modified_file_name)
 
 def main():
-    # Load the grib file 
+    # Load the original grib file 
     original_data = earthkit.data.from_source("file", "/users/clechart/clechart/neural-lam/laf2024042400")
-    # subset = original_data.sel(shortName=[x.lower() for x in constants.PARAM_NAMES_SHORT], level=constants.VERTICAL_LEVELS)
     subset = original_data.sel(shortName="u", level=constants.VERTICAL_LEVELS)
 
     # Load the array to replace the values with 
     replacement_data = np.load("/users/clechart/clechart/neural-lam/wandb/run-20240417_104748-dxnil3vw/files/results/inference/prediction_0.npy")
     original_cut = replacement_data[0,1,:,26:33].reshape(582,390, 7)
     cut_values = np.moveaxis(original_cut, [-3,-2,-1], [-1,-2,-3])
-    
-    # cut_values = np.flip(cut_values, axis=(0,1))
-    # cut_values = cut_values.reshape(cut_values.shape[0], -1)
-    # cut_values = cut_values.reshape(-1, cut_values.shape[0])
 
-    # # Ensure the dimensions match
-    # assert cut_values.shape == subset.values.shape, "The shapes of the arrays don't match."
+    # Create the modified GRIB file with the predicted data
+    modified_grib_path = "testinhoModif"
+    create_modified_grib(subset, cut_values, modified_grib_path)
 
-
-    # # Find indices where values are around -8
-    # close_to_minus_eight_subset = np.where((subset.values > -8.5) & (subset.values < -8.0))
-    # close_to_minus_eight_cut_values = np.where((cut_values > -8.5) & (cut_values < -8.0))
-
-    # print(f"Indices in subset.values close to -8: {close_to_minus_eight_subset}")
-    # print(f"Indices in cut_values close to -8: {close_to_minus_eight_cut_values}")
-
-    # Save the overwritten data 
-    md = subset.metadata()
-    # ni= md[0]["Ni"]
-    # nj = md[0]["Nj"]
-    data_new = earthkit.data.FieldList.from_array(cut_values, md) # .reshape(7, nj, ni)
-    data_new.save("testinhoModif")
-
-
-
-def pygrib_plotting():
-    # Load original GRIB data 
+    # Open the original and modified GRIB files
     original_grib = pygrib.open("/users/clechart/clechart/neural-lam/laf2024042400")
-    grb = original_grib.select(shortName = "u", level = 1)[0]
-    lats, lons = grb.latlons()
-    original_data = grb.values
+    grb_original = original_grib.select(shortName="u", level=constants.VERTICAL_LEVELS[0])[0]
 
-    # Load transformed GRIB with inference output
-    inference_grib = pygrib.open("testinhoModif")
-    inf = inference_grib.select(shortName = "u", level = 1)[0]
-    predicted_data = inf.values
+    predicted_grib = pygrib.open(modified_grib_path)
+    grb_predicted = predicted_grib.select(shortName="u", level=constants.VERTICAL_LEVELS[0])[0]
 
-    vmin = original_data.min()
-    vmax = original_data.max()
+    # Determine the global min and max values for the colorbar
+    vmin = min(grb_original.values.min(), grb_predicted.values.min())
+    vmax = max(grb_original.values.max(), grb_predicted.values.max())
 
-    # Setting up the map projection and the contour plot
-    fig, axes = plt.subplots(
-        2,
-        1,
-        figsize=constants.FIG_SIZE,
-        subplot_kw={"projection": constants.SELECTED_PROJ},
-    )
-    for axes, data in zip(axes, (original_data, predicted_data)):
-        contour_set = axes.contourf(
-            lons,
-            lats,
-            data,
-            transform=constants.SELECTED_PROJ,
-            cmap="plasma",
-            levels=np.linspace(vmin, vmax, num=100),
-        )
-        axes.add_feature(cf.BORDERS, linestyle="-", edgecolor="black")
-        axes.add_feature(cf.COASTLINE, linestyle="-", edgecolor="black")
-        axes.gridlines(
-            crs=constants.SELECTED_PROJ,
-            draw_labels=False,
-            linewidth=0.5,
-            alpha=0.5,
-        )
+    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(10, 12), subplot_kw={'projection': ccrs.PlateCarree()})
+    contour1 = plot_data(grb_original, "Original Data", ax1, ccrs.PlateCarree, vmin, vmax)
+    contour2 = plot_data(grb_predicted, "Predicted Data", ax2, ccrs.PlateCarree, vmin, vmax)
 
-    # Ticks and labels
-    # axes[0].set_title("Ground Truth", size=15)
-    # axes[1].set_title("Prediction", size=15)
-    cbar = fig.colorbar(contour_set, orientation="horizontal", aspect=20)
-    cbar.ax.tick_params(labelsize=10)
+    plt.subplots_adjust(hspace=0.1, wspace=0.05)
+    colorbar_ax = fig.add_axes([0.15, 0.08, 0.7, 0.02])  # Position for the colorbar
+    fig.colorbar(contour1, cax=colorbar_ax, orientation='horizontal', shrink=0.5)
 
-    # Save the plot to a file
-    plt.savefig("megamind_metadata.png", bbox_inches='tight')
+    plt.savefig("combined_vertical_data_adjusted.png", bbox_inches='tight')
     plt.close(fig)
 
 if __name__ == "__main__":
     main()
-    pygrib_plotting()
