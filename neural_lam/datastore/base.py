735d324d3b53f005861fe63add276d8d129c9432
Leif Denby <leif@denby.eu>
2024-09-10 18:46:25 +0200

    fix linting

--- neural_lam/datastore/base.py
+++ neural_lam/datastore/base.py
@@ -25,9 +25,9 @@ class BaseDatastore(abc.ABC):
     `torch.utils.data.Dataset` and uses the datastore to access the data).
 
     # Forecast vs analysis data
-    If the datastore is used to represent forecast rather than analysis data, then
-    the `is_forecast` attribute should be set to True, and returned data from
-    `get_dataarray` is assumed to have `analysis_time` and `forecast_time`
+    If the datastore is used to represent forecast rather than analysis data,
+    then the `is_forecast` attribute should be set to True, and returned data
+    from `get_dataarray` is assumed to have `analysis_time` and `forecast_time`
     dimensions (rather than just `time`).
 
     # Ensemble vs deterministic data
