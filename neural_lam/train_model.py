5fe9f845fadff8700b1ad4fec0ff7a20a85b7845
Kasper Hintz <kah@dmi.dk>
2025-01-21 16:02:16 +0000

    remove unused command line arguments

--- neural_lam/train_model.py
+++ neural_lam/train_model.py
@@ -242,12 +242,6 @@ def main(input_args=None):
         help="Number of example predictions to plot during evaluation "
         "(default: 1)",
     )
-    parser.add_argument(
-        "--save_predictions",
-        action="store_true",
-        help="If predictions should be saved to disk as a zarr dataset "
-        "(default: false)",
-    )
 
     # Logger Settings
     parser.add_argument(
