4a47c03723fe40a5997eb97a94337a0e7d068dc5
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-02-04 14:40:11 +0100

    add metrics watch to modelconfig

--- tests/conftest.py
+++ tests/conftest.py
@@ -128,6 +128,7 @@ def model_args():
         num_past_forcing_steps = 1
         num_future_forcing_steps = 1
         val_steps_to_log = [1, 3]
+        metrics_watch = []
 
     return ModelArgs()
 
