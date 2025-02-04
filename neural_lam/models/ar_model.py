2e46ecc1348c5db22a35ff3c3c2753ab66d80ebc
joeloskarsson <joel.oskarsson@liu.se>
2025-02-04 16:31:51 +0100

    Fix wrong scaling on step_diff

--- neural_lam/models/ar_model.py
+++ neural_lam/models/ar_model.py
@@ -65,11 +65,14 @@ class ARModel(pl.LightningModule):
             "state_std": torch.tensor(
                 da_state_stats.state_std.values, dtype=torch.float32
             ),
+            # Change stats below to be for diff of standardized variables
             "diff_mean": torch.tensor(
-                da_state_stats.state_diff_mean.values, dtype=torch.float32
+                da_state_stats.state_diff_mean.values / da_state_stats.state_std.values,
+                dtype=torch.float32
             ),
             "diff_std": torch.tensor(
-                da_state_stats.state_diff_std.values, dtype=torch.float32
+                da_state_stats.state_diff_std.values / da_state_stats.state_std.values,
+                dtype=torch.float32
             ),
         }
 
