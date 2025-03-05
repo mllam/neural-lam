3b0f24e37d7bdb6a5c76d3bfb592cc1ccbd6fd37
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-05 13:46:50 +0100

    add WarmupCosineAnnealingScheduler

--- tests/test_lr_scheduler.py
+++ tests/test_lr_scheduler.py
@@ -1,8 +1,6 @@
-# Standard library
-import warnings
-from unittest.mock import MagicMock
-
 # Third-party
+import matplotlib.pyplot as plt
+import numpy as np
 import pytest
 import torch
 
@@ -17,9 +15,43 @@ def model():
 
 @pytest.fixture
 def optimizer(model):
-    return torch.optim.SGD(model.parameters(), lr=0.01)  # Real optimizer
+    return torch.optim.Adam(model.parameters())  # Real optimizer
 
 
 def test_warmup_cosine_annealing_can_instantiate(optimizer):
-    lrs = lr_scheduler.WarmupCosineAnnealingLR(optimizer, max_steps=1000)
-    __import__("pdb").set_trace()  # TODO delme
+    min_factor = 0.001
+    max_factor = 1
+    warmup_steps = 10
+    annealing_steps = 10
+    initial_lr = optimizer.param_groups[0]["lr"]
+
+    linear = lr_scheduler.WarmupCosineAnnealingLR(
+        optimizer,
+        min_factor=min_factor,
+        max_factor=max_factor,
+        annealing_steps=annealing_steps,
+        warmup_steps=warmup_steps,
+    )
+
+    lrs = []
+    for _ in range(25):
+        lrs.append(optimizer.param_groups[0]["lr"])
+        linear.step()
+
+    expected_warmup_lr = np.linspace(
+        min_factor * initial_lr,
+        max_factor * initial_lr,
+        warmup_steps,
+        endpoint=False,
+    )
+    warmup_lr = lrs[:warmup_steps]
+    assert np.allclose(warmup_lr, expected_warmup_lr)
+
+    annealing_lr = lrs[warmup_steps : warmup_steps + annealing_steps]
+    expected_annealing_lr = min_factor * initial_lr + 0.5 * (
+        max_factor * initial_lr - min_factor * initial_lr
+    ) * (1 + np.cos(np.pi * np.arange(annealing_steps) / annealing_steps))
+    assert np.allclose(annealing_lr, expected_annealing_lr)
+
+    last_lr = lrs[-1]
+    assert all(lrs[-5:] == np.ones(5) * last_lr)
