2480d8bc3e8084ed45f2b8abe511792ff0efe05e
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-05 13:46:54 +0100

    add test

--- neural_lam/lr_scheduler.py
+++ neural_lam/lr_scheduler.py
@@ -1,8 +1,4 @@
-# Standard library
-import math
-
 # Third-party
-import matplotlib.pyplot as plt
 import torch
 
 
@@ -10,73 +6,43 @@ class WarmupCosineAnnealingLR(torch.optim.lr_scheduler.LRScheduler):
     def __init__(
         self,
         optimizer,
-        total_steps,
-        warmup_steps=1000,
-        max_lr=0.001,
-        min_lr=0.00001,
+        warmup_steps=10,
+        annealing_steps=90,
+        max_factor=1.0,
+        min_factor=0.001,
     ):
-        self.max_steps = max_steps
         self.warmup_steps = warmup_steps
-        self.max_lr = max_lr
-        self.min_lr = min_lr
-        schedule = MattsSchedule(
-            total_steps=100, warmup_steps=55, min_lr=0, max_lr=1
+        self.annealing_steps = annealing_steps
+        initial_learning_rate = optimizer.param_groups[0]["lr"]
+
+        self.warmup_scheduler = torch.optim.lr_scheduler.LinearLR(
+            optimizer,
+            start_factor=min_factor,
+            end_factor=max_factor,
+            total_iters=warmup_steps,
         )
+
+        self.annealing_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer,
+            T_max=annealing_steps,
+            eta_min=min_factor * initial_learning_rate,
+        )
+
         super().__init__(optimizer)
 
     def get_lr(self):
-        self.base_lrs
-        lrs = [1 for group in self.optimizer.param_groups]
-        return lrs
+        if self.last_epoch <= self.warmup_steps:
+            return self.warmup_scheduler.get_last_lr()
+        elif self.last_epoch <= self.warmup_steps + self.annealing_steps:
+            self.annealing_scheduler.step()
 
-    def warmup(self, step):
-        return step / self.warmup_steps * self.max
+        return True
 
-    def cosine_annealing(self, step):
-        if step > self.max_steps:
-            return self.min
-
-        return self.min + 0.5 * (self.max_lr - self.min_lr) * (
-            1 + math.cos(math.pi * step / self.max_steps)
-        )
-
-
-class MattsSchedule:
-    def __init__(
-        self,
-        total_steps,
-        warmup_steps=1000,
-        min_lr=0.00001,
-        max_lr=0.001,
-    ):
-        self.max_steps = total_steps
-        self.warmup_steps = warmup_steps
-        self.annealing_steps = total_steps - warmup_steps
-
-        self.max_lr = max_lr
-        self.min_lr = min_lr
-
-    def warmup(self, step):
-        return step / self.warmup_steps * self.max_lr
-
-    def cosine_annealing(self, step):
-        return self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (
-            1 + math.cos(math.pi * step / self.annealing_steps)
-        )
-
-    def calculate_lr(self, step):
-        if step < self.warmup_steps:
-            lr = self.warmup(step)
-        elif step < self.max_steps:
-            lr = self.cosine_annealing(step - self.warmup_steps)
-        else:
-            lr = self.min_lr
-        return lr
-
-    def get_lr(self, step):
-        __import__("pdb").set_trace()  # TODO delme kj:w
-
-        return [self.calculate_lr(step) for _ in optimizer.param_groups]
-
-    def __len__(self):
-        return self.max_steps
+    def step(self):
+        if self._step_count == 0:
+            pass
+        elif self._step_count <= self.warmup_steps:
+            self.warmup_scheduler.step()
+        elif self._step_count <= self.warmup_steps + self.annealing_steps:
+            self.annealing_scheduler.step()
+        self._step_count += 1
