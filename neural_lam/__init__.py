6a979f6289fda7130561e4659d683146dd35759e
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-06 11:18:52 +0100

    import trainer and lr scheduler

--- neural_lam/__init__.py
+++ neural_lam/__init__.py
@@ -3,8 +3,10 @@ import importlib.metadata
 
 # First-party
 import neural_lam.interaction_net
+import neural_lam.lr_scheduler
 import neural_lam.metrics
 import neural_lam.models
+import neural_lam.trainer
 import neural_lam.utils
 import neural_lam.vis
 
