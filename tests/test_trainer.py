31c22c1d668c90f20ae497cc09dc8887dad96e0d
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-06 11:19:51 +0100

    add test for trainer

---
+++ tests/test_trainer.py
@@ -0,0 +1,20 @@
+# Third-party
+import torch
+
+# First-party
+import neural_lam
+
+
+def test_trainer_instantiates():
+    neural_lam.trainer.Trainer()
+
+
+def test_configure_optimizer_factory(model):
+    trainer = neural_lam.trainer.Trainer()
+    configure_optimizers = trainer.configure_optimizers_factory()
+    [optimizer], [scheduler] = configure_optimizers(model)
+
+    assert isinstance(optimizer, torch.optim.Adam)
+    assert isinstance(
+        scheduler, neural_lam.lr_scheduler.WarmupCosineAnnealingLR
+    )
