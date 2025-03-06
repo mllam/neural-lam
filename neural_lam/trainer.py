6afa437ff3a59aee8c43c3e53dbf6b49a43cfe53
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-06 11:19:17 +0100

    add trainer class with configure optimizers

---
+++ neural_lam/trainer.py
@@ -0,0 +1,42 @@
+# Standard library
+from types import MethodType
+
+# Third-party
+import pytorch_lightning as pl
+import torch
+
+# First-party
+import neural_lam
+
+
+class Trainer(pl.Trainer):
+    def __init__(
+        self, scheduler_config=None, optimizer_config=None, *args, **kwargs
+    ):
+        self.scheduler_config = scheduler_config
+        self.optimizer_config = optimizer_config
+        super().__init__(*args, **kwargs)
+
+    def fit(
+        self,
+        model,
+        *args,
+        **kwargs,
+    ):
+        model.configure_optimizers = MethodType(
+            self.get_configure_optimizers_callback(), model
+        )
+        super().fit(model, *args, **kwargs)
+
+    def configure_optimizers_factory(self):
+
+        # TODO configure optimizer and scheduler from member config files
+
+        def configure_optimizers(pl_module):
+            optimizer = torch.optim.Adam(pl_module.parameters())
+            scheduler = neural_lam.lr_scheduler.WarmupCosineAnnealingLR(
+                optimizer
+            )
+            return [optimizer], [scheduler]
+
+        return configure_optimizers
