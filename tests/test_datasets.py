1b2bb4fdd33339a7fff22dd75ae52915dd368596
Simon Adamov <kode@mailbox.org>
2026-02-26 09:55:36 +0100

    removed unused import

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -13,7 +13,7 @@ from neural_lam.create_graph import create_graph_from_datastore
 from neural_lam.datastore import DATASTORES
 from neural_lam.datastore.base import BaseRegularGridDatastore
 from neural_lam.models.graph_lam import GraphLAM
-from neural_lam.weather_dataset import WeatherDataModule, WeatherDataset
+from neural_lam.weather_dataset import WeatherDataset
 from tests.conftest import init_datastore_example
 from tests.dummy_datastore import DummyDatastore
 
