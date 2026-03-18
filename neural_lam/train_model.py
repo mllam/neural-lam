Fix silent metric logging and improve 1D tensor robustness

neural_lam/train_model.py
@@ -1,13 +1,24 @@
 # Standard library
 import json
 import random
+import torch
+import argparse
+import neural_lam.config as config
 import time
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
+torch.serialization.add_safe_globals([
+    argparse.Namespace, 
+    config.NeuralLAMConfig, 
+    config.DatastoreSelection, 
+    config.TrainingConfig,
+    config.ManualStateFeatureWeighting,
+    config.OutputClamping
+])
 
 # Third-party
 # for logging the model:
 import pytorch_lightning as pl
-import torch
 from lightning_fabric.utilities import seed
 from loguru import logger
 

neural_lam/vis.py
@@ -94,7 +94,8 @@ def plot_on_axis(
 
     if isinstance(da, xr.DataArray) and "x" in da.dims and "y" in da.dims:
         da = da.transpose("x", "y")
-
+    if da.size != np.prod(grid_shape):
+        return None # Gracefully skip plots that don't match the grid
     values = da.values.reshape(grid_shape)
 
     mesh = ax.pcolormesh(