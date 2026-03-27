ea4ba0d773af791eefba229b8a4d07323b9e065c
Chaitanya Parab <cparab02@gmail.com>
2026-03-27 12:27:01 +0530

    Add initial Sphinx documentation setup with API reference

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'neural-lam'
+copyright = '2026, MLLAM'
+author = 'MLLAM'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,19 @@
+.. neural-lam documentation master file, created by
+   sphinx-quickstart on Fri Mar 27 11:44:07 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Neural-LAM Documentation
+========================
+
+Welcome to the official documentation for Neural-LAM.
+
+This documentation includes API references and will be extended with tutorials,
+installation guides, and usage examples.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
+
