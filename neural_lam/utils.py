bf1102a95eab9e4e96a4774a9fcc1c0e3a2d2ea9
Shyam Saini <shyamsaini1530@gmail.com>
2026-04-09 12:57:11 +0530

    Wandb logger and MLflow logger into runs  also add fallback in Mlflow logger

--- neural_lam/utils.py
+++ neural_lam/utils.py
@@ -341,7 +341,8 @@ def init_training_logger_metrics(training_logger, val_steps):
 
 
 @rank_zero_only
-def setup_training_logger(datastore, args, run_name):
+def setup_training_logger(datastore, args, run_name,run_dir):
+    print("LOGGER TYPE:",args.logger)
     """Set up the training logger (WandB or MLFlow).
 
     Parameters
@@ -385,6 +386,7 @@ def setup_training_logger(datastore, args, run_name):
             config=dict(training=vars(args), datastore=datastore._config),
             resume=wandb_resume,
             id=args.wandb_id,
+            save_dir= run_dir,
         )
     elif args.logger == "mlflow":
         if args.wandb_id is not None:
@@ -394,13 +396,16 @@ def setup_training_logger(datastore, args, run_name):
             )
         url = os.getenv("MLFLOW_TRACKING_URI")
         if url is None:
-            raise ValueError(
-                "MLFlow logger requires setting MLFLOW_TRACKING_URI in env."
-            )
+            logger.warning("MLFLOW_TRACKING_URI not set. falling back to local run directory.")
+            # raise ValueError(
+                #  "MLFlow logger requires setting MLFLOW_TRACKING_URI in env."
+            #  )
+        url = f"file:///{run_dir}"
         training_logger = CustomMLFlowLogger(
             experiment_name=args.logger_project,
             tracking_uri=url,
             run_name=run_name,
+            save_dir= run_dir
         )
         training_logger.log_hyperparams(
             dict(training=vars(args), datastore=datastore._config)
