b595565ba80ad7b845c5a2e002730fe08353d54c
sadamov <kode@mailbox.org>
2026-04-16 10:04:40 +0200

    make shim more robust

--- neural_lam/datastore/plot_example.py
+++ neural_lam/datastore/plot_example.py
@@ -80,7 +80,7 @@ def plot_example_from_datastore(
         transform=crs,
         size=4,
     )
-    for ax in getattr(g, "axs", g.axes).flat:
+    for ax in (g.axs if hasattr(g, "axs") else g.axes).flat:
         ax.coastlines()
         ax.gridlines(draw_labels=["left", "bottom"])
         ax.set_extent(datastore.get_xy_extent(category=category), crs=crs)
